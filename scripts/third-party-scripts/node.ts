e63eaa05fdf799eed130f78ff6279b4e82afa615
oneofthezombies <hunhoekim@gmail.com>
2023-06-19 23:51:48 +0900

    build: add clean env

--- scripts/third-party-scripts/node.ts
+++ scripts/third-party-scripts/node.ts
@@ -1,15 +1,28 @@
 import child_process from 'child_process';
 import path from 'path';
 
+export function newCleanNodeEnv(): NodeJS.ProcessEnv {
+  const newEnv: NodeJS.ProcessEnv = {};
+  Object.keys(process.env).forEach((key) => {
+    if (key.toLowerCase().startsWith('node_')) return;
+    if (key.toLowerCase().startsWith('npm_')) return;
+    if (key.toLowerCase().startsWith('nvm_')) return;
+    if (key.toLowerCase().startsWith('pnpm_')) return;
+    newEnv[key] = process.env[key];
+  });
+  return newEnv;
+}
+
 export function enableCorepack(nodeOutPath: string): void {
   const binPath = process.platform === 'win32' ? path.resolve(nodeOutPath) : path.resolve(nodeOutPath, 'bin');
   const corepackPath = process.platform === 'win32' ? path.resolve(binPath, 'corepack.cmd') : path.resolve(binPath, 'corepack');
+  const env = newCleanNodeEnv();
   const proc = child_process.spawnSync(corepackPath, ['enable'], {
     cwd: binPath,
     stdio: 'inherit',
     env: {
-      ...process.env,
-      PATH: `${binPath}${path.delimiter}${process.env.PATH ?? ''}`,
+      ...env,
+      PATH: `${binPath}${path.delimiter}${env.PATH ?? ''}`,
     },
   });
   proc.error && console.error(proc.error);
