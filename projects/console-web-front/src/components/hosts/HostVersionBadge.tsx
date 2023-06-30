3f02bf8b3a70253fd89533f9fff3f926228a09c8
Daniel Ahn <daniel@dogutech.io>
2023-06-30 16:33:22 +0900

    feat: dogu version

--- projects/console-web-front/src/components/hosts/HostVersionBadge.tsx
+++ projects/console-web-front/src/components/hosts/HostVersionBadge.tsx
@@ -1,16 +1,28 @@
 import { QuestionCircleFilled } from '@ant-design/icons';
-import { Tag } from 'antd';
+import { Tag, Tooltip } from 'antd';
 
 interface Props {
   version: string | null;
 }
 
 const HostVesrsionBadge = ({ version }: Props) => {
+  const currentVersion = process.env.NEXT_PUBLIC_DOGU_VERSION;
+  // isMatched should be true when major, minor version is same
+  const isMatched = currentVersion?.split('.').slice(0, 2).join('.') === version?.split('.').slice(0, 2).join('.');
+
   if (!version) {
-    return <Tag color="red">N/A</Tag>;
+    return <Tag color="default">N/A</Tag>;
   }
 
-  return <Tag color="blue">{version}</Tag>;
+  return (
+    <Tooltip
+      title={`Dost and Dogu version not matched!\nThis can result in unexpected behavior.\nDogu: ${currentVersion}, Dost: ${version}`}
+      open={isMatched ? false : undefined}
+      overlayInnerStyle={{ fontSize: '.8rem', textAlign: 'center' }}
+    >
+      <Tag color={isMatched ? 'blue' : 'warning'}>{version}</Tag>
+    </Tooltip>
+  );
 };
 
 export default HostVesrsionBadge;
