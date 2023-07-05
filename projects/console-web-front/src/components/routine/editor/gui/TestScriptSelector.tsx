112d782c5bfd88d0cfe9a9c020cd57df6c20963c
Daniel Ahn <daniel@dogutech.io>
2023-07-05 19:35:29 +0900

    feat: file icons

--- projects/console-web-front/src/components/routine/editor/gui/TestScriptSelector.tsx
+++ projects/console-web-front/src/components/routine/editor/gui/TestScriptSelector.tsx
@@ -1,11 +1,13 @@
 import useSWR from 'swr';
 import { OrganizationId, ProjectId, ProjectTestScript } from '@dogu-private/types';
-import { RepositoryFileMetaTree } from '@dogu-private/console';
+import { FileOutlined } from '@ant-design/icons';
 import { Select, SelectProps } from 'antd';
 import styled from 'styled-components';
+import Image from 'next/image';
 
 import { swrAuthFetcher } from '../../../../api';
 import { flexRowBaseStyle } from '../../../../styles/box';
+import resources from '../../../../resources';
 
 interface Props extends Omit<SelectProps, 'options'> {
   organizationId: OrganizationId;
@@ -17,11 +19,28 @@ const TestScriptSelector = ({ organizationId, projectId, ...props }: Props) => {
     revalidateOnFocus: false,
   });
 
+  const getFileIcon = (name: string) => {
+    const extension = name.split('.').pop();
+
+    switch (extension) {
+      case 'js':
+        return <Image src={resources.icons.javascript} width={16} height={16} alt="javascript" style={{ display: 'flex' }} />;
+      case 'ts':
+        return <Image src={resources.icons.typescript} width={16} height={16} alt="typescript" style={{ display: 'flex' }} />;
+      case 'py':
+        return <Image src={resources.icons.python} width={16} height={16} alt="python" style={{ display: 'flex' }} />;
+      default:
+        return <FileOutlined />;
+    }
+  };
+
   const options: SelectProps['options'] = data?.map((item) => {
     return {
       label: (
         <TestScriptBox>
-          <TestScriptName>{item.name}</TestScriptName>
+          <TestScriptName>
+            {getFileIcon(item.name)}&nbsp;{item.name}
+          </TestScriptName>
           <TestScriptPath>{`(${item.path})`}</TestScriptPath>
         </TestScriptBox>
       ),
@@ -37,9 +56,15 @@ export default TestScriptSelector;
 const TestScriptBox = styled.div``;
 
 const TestScriptName = styled.b`
+  ${flexRowBaseStyle}
   font-size: 0.875rem;
   margin-right: 0.25rem;
   font-weight: 500;
+
+  rect {
+    width: 12px;
+    height: 12px;
+  }
 `;
 
 const TestScriptPath = styled.p`
