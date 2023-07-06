87f4431797dce3cf9ebf614a1ab3fed409b0ebdf
dev-kyun <gamekyun4@gmail.com>
2023-07-06 10:52:39 +0900

    update: project scm auth db migration

--- projects/console-web-server/src/db/migrations/local/1688608048172-typeorm-migration.ts
+++ projects/console-web-server/src/db/migrations/local/1688608048172-typeorm-migration.ts
@@ -1,17 +1,17 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class typeormMigration1688540992872 implements MigrationInterface {
-  name = 'typeormMigration1688540992872';
+export class typeormMigration1688608048172 implements MigrationInterface {
+  name = 'typeormMigration1688608048172';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
       `CREATE TABLE "project_scm" ("project_scm_id" uuid NOT NULL, "project_id" uuid NOT NULL, "type" smallint NOT NULL DEFAULT '0', "url" character varying NOT NULL, "created_at" TIMESTAMP(3) WITH TIME ZONE NOT NULL DEFAULT ('now'::text)::timestamp(3) with time zone, "updated_at" TIMESTAMP(3) WITH TIME ZONE NOT NULL DEFAULT ('now'::text)::timestamp(3) with time zone, "deleted_at" TIMESTAMP(3) WITH TIME ZONE DEFAULT NULL, CONSTRAINT "PK_60734f767a902afbc534fae2807" PRIMARY KEY ("project_scm_id"))`,
     );
     await queryRunner.query(
-      `CREATE TABLE "project_scm_github_auth" ("project_scm_github_auth_id" uuid NOT NULL, "project_scm_id" uuid NOT NULL, "token" character varying, "created_at" TIMESTAMP(3) WITH TIME ZONE NOT NULL DEFAULT ('now'::text)::timestamp(3) with time zone, "updated_at" TIMESTAMP(3) WITH TIME ZONE NOT NULL DEFAULT ('now'::text)::timestamp(3) with time zone, "deleted_at" TIMESTAMP(3) WITH TIME ZONE DEFAULT NULL, CONSTRAINT "PK_a2ff0b992989276944770bd0d7d" PRIMARY KEY ("project_scm_github_auth_id"))`,
+      `CREATE TABLE "project_scm_github_auth" ("project_scm_github_auth_id" uuid NOT NULL, "project_scm_id" uuid NOT NULL, "token" character varying NOT NULL, "created_at" TIMESTAMP(3) WITH TIME ZONE NOT NULL DEFAULT ('now'::text)::timestamp(3) with time zone, "updated_at" TIMESTAMP(3) WITH TIME ZONE NOT NULL DEFAULT ('now'::text)::timestamp(3) with time zone, "deleted_at" TIMESTAMP(3) WITH TIME ZONE DEFAULT NULL, CONSTRAINT "PK_a2ff0b992989276944770bd0d7d" PRIMARY KEY ("project_scm_github_auth_id"))`,
     );
     await queryRunner.query(
-      `CREATE TABLE "project_scm_gitlab_auth" ("project_scm_gitlab_auth_id" uuid NOT NULL, "project_scm_id" uuid NOT NULL, "token" character varying, "created_at" TIMESTAMP(3) WITH TIME ZONE NOT NULL DEFAULT ('now'::text)::timestamp(3) with time zone, "updated_at" TIMESTAMP(3) WITH TIME ZONE NOT NULL DEFAULT ('now'::text)::timestamp(3) with time zone, "deleted_at" TIMESTAMP(3) WITH TIME ZONE DEFAULT NULL, CONSTRAINT "PK_3a6597447b141647e4d80f4e2b4" PRIMARY KEY ("project_scm_gitlab_auth_id"))`,
+      `CREATE TABLE "project_scm_gitlab_auth" ("project_scm_gitlab_auth_id" uuid NOT NULL, "project_scm_id" uuid NOT NULL, "token" character varying NOT NULL, "created_at" TIMESTAMP(3) WITH TIME ZONE NOT NULL DEFAULT ('now'::text)::timestamp(3) with time zone, "updated_at" TIMESTAMP(3) WITH TIME ZONE NOT NULL DEFAULT ('now'::text)::timestamp(3) with time zone, "deleted_at" TIMESTAMP(3) WITH TIME ZONE DEFAULT NULL, CONSTRAINT "PK_3a6597447b141647e4d80f4e2b4" PRIMARY KEY ("project_scm_gitlab_auth_id"))`,
     );
     await queryRunner.query(
       `ALTER TABLE "project_scm" ADD CONSTRAINT "FK_b15c91377e14c140cd3b65c8da6" FOREIGN KEY ("project_id") REFERENCES "project"("project_id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
