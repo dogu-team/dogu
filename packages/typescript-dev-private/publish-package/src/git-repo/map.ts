8214387770be66807544bd1bdc94cff4df34a3ea
oneofthezombies <hunhoekim@gmail.com>
2023-07-07 20:22:52 +0900

    chore: add package github map

--- packages/typescript-dev-private/publish-package/src/git-repo/map.ts
+++ packages/typescript-dev-private/publish-package/src/git-repo/map.ts
@@ -13,6 +13,10 @@ export const PackageGithubMap: PackageGithubMap = {
     development: `https://oneofthezombies:${process.env.DOGU_ACTIONS_PUBLISH_TOKEN}@github.com/dogu-actions/prepare.git`,
     production: `https://oneofthezombies:${process.env.DOGU_ACTIONS_PUBLISH_TOKEN}@github.com/dogu-actions/prepare.git`,
   },
+  '@dogu-actions/checkout': {
+    development: `https://oneofthezombies:${process.env.DOGU_ACTIONS_PUBLISH_TOKEN}@github.com/dogu-actions/checkout.git`,
+    production: `https://oneofthezombies:${process.env.DOGU_ACTIONS_PUBLISH_TOKEN}@github.com/dogu-actions/checkout.git`,
+  },
   '@dogu-user-templates/typescript-template': {
     development: `https://root:${process.env.DOGU_GITLAB_ROOT_TOKEN}@gitlab.dev.dogutech.io/dogu/typescript-template.git`,
     production: `https://root:${process.env.DOGU_GITLAB_ROOT_TOKEN}@gitlab.dogutech.io/dogu/typescript-template.git`,
