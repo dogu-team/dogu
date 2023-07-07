2d020bc08b65c599d8e1dab3b8e448e2478ea924
dev-kyun <gamekyun4@gmail.com>
2023-07-07 15:51:20 +0900

    update: webdriveragent guard

--- projects/console-web-server/src/module/webdriver/webdriver.controller.ts
+++ projects/console-web-server/src/module/webdriver/webdriver.controller.ts
@@ -1,6 +1,8 @@
 import { RelayResponse } from '@dogu-tech/device-client-common';
 import { All, Controller, Delete, Post, Req, Res } from '@nestjs/common';
 import { Request, Response } from 'express';
+import { API_TOKEN_TYPE } from '../auth/auth.types';
+import { ApiTokenPermission } from '../auth/decorators';
 import { WebDriverService } from './webdriver.service';
 
 @Controller('/wd/hub')
@@ -8,6 +10,7 @@ export class WebDriverController {
   constructor(private readonly webdriverService: WebDriverService) {}
 
   @Post('session')
+  @ApiTokenPermission(API_TOKEN_TYPE.WEBDRIVER_AGENT)
   async newSession(@Req() request: Request, @Res() response: Response): Promise<void> {
     const relayResponse = await this.webdriverService.process(request, response);
     this.sendResponse(relayResponse, response);
@@ -20,6 +23,7 @@ export class WebDriverController {
   }
 
   @All('session/:sessionId/*')
+  // @ApiTokenPermission(API_TOKEN_TYPE.WEBDRIVER_AGENT)
   async process(@Req() request: Request, @Res() response: Response): Promise<void> {
     const relayResponse = await this.webdriverService.process(request, response);
     this.sendResponse(relayResponse, response);
