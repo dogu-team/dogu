1583c4fcba100b5a2b9d15f3dad645678b1d42c5
Byoungwook Park <qkrquddnr99@gmail.com>
2023-07-12 10:59:39 +0900

    fix: poop: skip manual setup

--- nm-space/projects/dost/src/pages/SetupManual.tsx
+++ nm-space/projects/dost/src/pages/SetupManual.tsx
@@ -1,53 +1,56 @@
-import { Button, Divider, Flex, List, Spinner } from '@chakra-ui/react';
-import { useEffect, useLayoutEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-
-import ManualExternalToolValidCheckerItem from '../components/external/ManualExternalToolValidCheckerItem';
-import PageTitle from '../components/layouts/PageTitle';
-import useManualSetupExternalValidResult from '../hooks/manaul-setup-external-valid-result';
 import useEnvironmentStore from '../stores/environment';
-import { ipc } from '../utils/window';
+// import { Button, Divider, Flex, List, Spinner } from '@chakra-ui/react';
+// import { useEffect, useLayoutEffect, useState } from 'react';
+
+// import ManualExternalToolValidCheckerItem from '../components/external/ManualExternalToolValidCheckerItem';
+// import PageTitle from '../components/layouts/PageTitle';
+// import useManualSetupExternalValidResult from '../hooks/manaul-setup-external-valid-result';
+// import { ipc } from '../utils/window';
 
 const SetupManual = () => {
   const { useApiUrlInput } = useEnvironmentStore((state) => state.features);
-  const { results, loading, validate } = useManualSetupExternalValidResult();
+  // const { results, loading, validate } = useManualSetupExternalValidResult();
   const navigate = useNavigate();
 
-  if (!loading && !results) {
-    return <div>Error occured while checking...</div>;
-  }
+  navigate(useApiUrlInput ? '/setup/config' : '/home/connect');
+  return <div></div>;
 
-  return (
-    <Flex direction="column" style={{ padding: '24px', height: '100%' }}>
-      <PageTitle title="Manual Setup" />
+  // if (!loading && !results) {
+  //   return <div>Error occured while checking...</div>;
+  // }
 
-      <Divider mt={6} mb={6} />
+  // return (
+  //   <Flex direction="column" style={{ padding: '24px', height: '100%' }}>
+  //     <PageTitle title="Manual Setup" />
 
-      <Flex direction="column" justifyContent="space-between" flex={1}>
-        {results ? (
-          <div>
-            <List spacing={2} width="100%">
-              {results.length ? (
-                results.map((result) => {
-                  return <ManualExternalToolValidCheckerItem key={result.key} isValid={result.isValid} externalKey={result.key} name={result.name} onValidateEnd={validate} />;
-                })
-              ) : (
-                <div>Nothing to do.</div>
-              )}
-            </List>
-          </div>
-        ) : (
-          <Spinner />
-        )}
+  //     <Divider mt={6} mb={6} />
 
-        <Flex justifyContent="flex-end">
-          <Button onClick={() => navigate(useApiUrlInput ? '/setup/config' : '/home/connect')} isDisabled={!results?.every((item) => item.isValid)} colorScheme="blue">
-            {useApiUrlInput ? 'Continue' : 'Finish'}
-          </Button>
-        </Flex>
-      </Flex>
-    </Flex>
-  );
+  //     <Flex direction="column" justifyContent="space-between" flex={1}>
+  //       {results ? (
+  //         <div>
+  //           <List spacing={2} width="100%">
+  //             {results.length ? (
+  //               results.map((result) => {
+  //                 return <ManualExternalToolValidCheckerItem key={result.key} isValid={result.isValid} externalKey={result.key} name={result.name} onValidateEnd={validate} />;
+  //               })
+  //             ) : (
+  //               <div>Nothing to do.</div>
+  //             )}
+  //           </List>
+  //         </div>
+  //       ) : (
+  //         <Spinner />
+  //       )}
+
+  //       <Flex justifyContent="flex-end">
+  //         <Button onClick={() => navigate(useApiUrlInput ? '/setup/config' : '/home/connect')} isDisabled={!results?.every((item) => item.isValid)} colorScheme="blue">
+  //           {useApiUrlInput ? 'Continue' : 'Finish'}
+  //         </Button>
+  //       </Flex>
+  //     </Flex>
+  //   </Flex>
+  // );
 };
 
 export default SetupManual;
