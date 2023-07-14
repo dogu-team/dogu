7505611b13c153aafd3d5f0358ffd34c2a042805
dev-kyun <gamekyun4@gmail.com>
2023-07-14 22:16:59 +0900

    update: enable s3 file server

--- projects/console-web-server/src/module/feature/file/feature-file.module.ts
+++ projects/console-web-server/src/module/feature/file/feature-file.module.ts
@@ -1,10 +1,13 @@
 import { ClassProvider, Module } from '@nestjs/common';
+import { FeatureConfig } from '../../../feature.config';
 import { FeatureFileService } from './feature-file.service';
 import { NexusFeatureFileService } from './implementations/nexus-feature-file.service';
+import { S3FeatureFileService } from './implementations/s3-feature-file.service';
 
+const FeatureFileServiceValue = FeatureConfig.get('fileService');
 const FeatureFileServiceProvider: ClassProvider = {
   provide: FeatureFileService,
-  useClass: NexusFeatureFileService,
+  useClass: FeatureFileServiceValue === 's3' ? S3FeatureFileService : NexusFeatureFileService,
 };
 
 @Module({
