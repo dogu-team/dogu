e1b647fe5cb658d8fb1993bd4e1ae7154f32fcdc
Daniel Ahn <daniel@dogutech.io>
2023-07-14 11:46:20 +0900

    feat: enable swc and styled-component compiler

--- projects/console-web-front/next.config.mjs
+++ projects/console-web-front/next.config.mjs
@@ -42,6 +42,7 @@ const nextConfig = {
     locales: ['en', 'ko'],
     defaultLocale: 'en',
   },
+  swcMinify: true,
   headers: async () => {
     return process.env.NEXT_PUBLIC_ENV === 'production' || process.env.NEXT_PUBLIC_ENV === 'development'
       ? [
@@ -56,6 +57,9 @@ const nextConfig = {
           },
         ]
       : [];
+  },
+  compiler: {
+    styledComponents: true,
   },
   sentry: {
     disableServerWebpackPlugin: true,
