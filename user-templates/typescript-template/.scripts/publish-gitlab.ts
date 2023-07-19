9c4bfea6eba31645025b7818d962e74381ac4e93
Seong JuWon <lumyjuwon@gmail.com>
2023-07-19 17:35:06 +0900

    chore: delete not used

--- user-templates/typescript-template/.scripts/publish-gitlab.ts
+++
@@ -1,9 +0,0 @@
-import { GitlabTemplatePublisher } from '@dogu-dev-private/publish-package';
-
-(async (): Promise<void> => {
-  const publisher = new GitlabTemplatePublisher();
-  await publisher.publish();
-})().catch((error) => {
-  console.error(error);
-  process.exit(1);
-});
