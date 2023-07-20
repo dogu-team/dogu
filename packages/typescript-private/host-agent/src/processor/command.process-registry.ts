ae7cc4f9cd7504be3f22d4ed1d797ae5e186a844
Byoungwook Park <qkrquddnr99@gmail.com>
2023-07-20 12:47:37 +0900

    logs: command log

--- packages/typescript-private/host-agent/src/processor/command.process-registry.ts
+++ packages/typescript-private/host-agent/src/processor/command.process-registry.ts
@@ -21,35 +21,50 @@ export class CommandProcessRegistry {
   constructor(private readonly logger: DoguLogger) {}
 
   async command(command: string, args: string[], options: SpawnOptions, context: MessageContext): Promise<ErrorResult> {
+    this.logger.info('command 1', { command, args, options });
     return new Promise((resolve, reject) => {
       (async (): Promise<void> => {
+        this.logger.info('command 2', { command, args, options });
         const { processes } = this;
         const { env: childEnv, ...rest } = options;
         const { environmentVariableReplacer, eventHandler } = context;
+        this.logger.info('command 3', { command, args, options });
         if (childEnv) {
+          this.logger.info('command 4', { command, args, options });
           const childEnvReplaced = await environmentVariableReplacer.replaceEnv(childEnv);
           environmentVariableReplacer.stackProvider.push(new EnvironmentVariableReplacementProvider(childEnvReplaced));
+          this.logger.info('command 5', { command, args, options });
         }
+        this.logger.info('command 6', { command, args, options });
         const commandReplaced = await environmentVariableReplacer.replace(command);
+        this.logger.info('command 7', { command, args, options });
         const argsReplaced = await Promise.all(args.map((arg) => environmentVariableReplacer.replace(arg)));
+        this.logger.info('command 8', { command, args, options });
         const env = environmentVariableReplacer.stackProvider.export();
+        this.logger.info('command 9', { command, args, options });
         if (rest.cwd) {
+          this.logger.info('command 10', { command, args, options });
           if (!fs.existsSync(rest.cwd)) {
+            this.logger.info('command 11', { command, args, options });
             this.logger.error('child process cwd not found.', { cwd: rest.cwd });
           }
         }
+        this.logger.info('command 12', { command, args, options });
         const child = spawn(commandReplaced, argsReplaced, {
           ...rest,
           env,
         });
+        this.logger.info('command 13', { command, args, options });
         processes.add(child);
         let cancelRequested = false;
         const canceler: MessageCanceler = {
           cancel: () => {
+            this.logger.info('command 100', { command, args, options });
             cancelRequested = true;
             child.kill();
           },
         };
+        this.logger.info('command 14', { command, args, options });
         child.on('spawn', () => {
           this.logger.info('child process spawned', { command: commandReplaced, args: argsReplaced });
         });
