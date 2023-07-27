d0f617630c46e846e595dfdafc0e5f3f9bdaeb9d
dev-kyun <gamekyun4@gmail.com>
2023-07-27 22:40:43 +0900

    update: local influxdb start

--- projects/console-web-server/scripts/start-influxdb/run.ts
+++ projects/console-web-server/scripts/start-influxdb/run.ts
@@ -1,6 +1,6 @@
 import { node_package } from '@dogu-dev-private/build-tools';
 import { checkDockerInstalled, clearDokerContainer, clearVolume, createVolume, pullDockerImage } from '../common/common';
-import { exec, execute, which } from '../utils/utils';
+import { exec, execute } from '../utils/utils';
 import { config } from './config';
 
 async function startDockerContainer() {
@@ -30,19 +30,6 @@ async function startDockerContainer() {
   const currentDir = process.cwd();
   const workspaceDir = node_package.findRootWorkspace();
   process.chdir(workspaceDir);
-
-  await execute('Checking Docker...', () => which('docker', { errorMessage: 'Error: Docker is not installed' }));
-  await execute('Pulling image...', () => exec('docker pull postgres', { errorMessage: 'Error: Docker pull failed', retry: true }));
-  await execute('Stopping container...', async () => {
-    const { stdout } = await exec(`docker ps -a --filter name=${config.containerName} --format "{{.ID}}"`, { errorMessage: 'Error: Docker ps failed' });
-    const containerId = stdout.trim();
-    if (containerId) {
-      console.log(`Container found: ${containerId}`);
-      await execute('Removing container...', () => exec(`docker rm -f ${containerId}`, { errorMessage: 'Error: Docker remove failed' }));
-    } else {
-      console.log('Container not found');
-    }
-  });
 
   await clearDokerContainer(config.containerName);
   await clearVolume(config.volumeName);
