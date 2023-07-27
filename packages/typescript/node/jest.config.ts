1a52f6a149d3f22ec4c489e87b1b53f79810ada3
oneofthezombies <hunhoekim@gmail.com>
2023-07-27 18:15:27 +0900

    chore: add test roots

--- packages/typescript/node/jest.config.ts
+++ packages/typescript/node/jest.config.ts
@@ -2,6 +2,7 @@ import type { JestConfigWithTsJest } from 'ts-jest';
 
 const jestConfig: JestConfigWithTsJest = {
   preset: 'ts-jest',
+  roots: ['<rootDir>/src'],
 };
 
 export default jestConfig;
