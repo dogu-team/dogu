075f6f986f53c0662b6525c78c8739d493e4eb23
dev-kyun <gamekyun4@gmail.com>
2023-07-28 22:01:52 +0900

    fix: prod db migration

--- projects/console-web-server/src/migration.config.ts
+++ projects/console-web-server/src/migration.config.ts
@@ -1,16 +1,12 @@
 import { stringify } from '@dogu-tech/common';
 import { DataSource } from 'typeorm';
 import { dataSourceConfig } from './config';
-import { env } from './env';
 import { logger } from './module/logger/logger.instance';
 
 const datasource = new DataSource(dataSourceConfig); // config is one that is defined in datasource.config.ts file
 datasource
   .initialize()
   .then((value) => {
-    if (env.NODE_ENV === 'production' || env.DOGU_RUN_TYPE === 'production') {
-      throw new Error('production mode is not supported.');
-    }
     logger.info(`typeorm options: ${stringify(value.options)}`);
   })
   .catch((e) => {
