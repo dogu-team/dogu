abc662b8ceb47a8112d6626bbea24f41f82eb73c
dev-kyun <gamekyun4@gmail.com>
2023-07-28 15:11:38 +0900

    update: e2e script

--- e2e/test/bat/bat-routine.test.ts
+++ e2e/test/bat/bat-routine.test.ts
@@ -83,6 +83,7 @@ Dest.withOptions({
     beforeAll(async () => {
       values.value.HOME_URL = `http://${env.DOGU_E2E_HOST}:${env.DOGU_CONSOLE_WEB_FRONT_PORT}`;
       console.log(`DeviceServerPort ${env.DOGU_DEVICE_SERVER_PORT}`);
+      await ProcessManager.killByPorts([Number(env.DOGU_CONSOLE_WEB_FRONT_PORT)]);
       await ProcessManager.killByNames(['adb']);
     });
 
