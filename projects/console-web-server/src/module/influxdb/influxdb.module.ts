7d9e288afb20a5c70a7c987bebec0b29d761c27e
oneofthezombies <hunhoekim@gmail.com>
2023-07-31 12:18:43 +0900

    chore: remove unused files

--- projects/console-web-server/src/module/influxdb/influxdb.module.ts
+++ projects/console-web-server/src/module/influxdb/influxdb.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { InfluxDbDeviceService } from './influxdb-device.service';
-import { InfluxDbHostService } from './influxdb-host.service';
 import { InfluxDbLogService } from './influxdb-log.service';
 import { InfluxDbApi } from './influxdb.api';
 import { InfluxDbQuerier } from './influxdb.querier';
 import { InfluxDbWriter } from './influxdb.writer';
 
 @Module({
-  providers: [InfluxDbApi, InfluxDbQuerier, InfluxDbWriter, InfluxDbHostService, InfluxDbDeviceService, InfluxDbLogService],
-  exports: [InfluxDbHostService, InfluxDbDeviceService, InfluxDbLogService],
+  providers: [InfluxDbApi, InfluxDbQuerier, InfluxDbWriter, InfluxDbDeviceService, InfluxDbLogService],
+  exports: [InfluxDbDeviceService, InfluxDbLogService],
 })
 export class InfluxDbModule {}
