e216ae36879aaf8f3fdaa0a70d432cb3e8404dc2
Daniel Ahn <daniel@dogutech.io>
2023-08-05 17:41:34 +0900

    fix: external guide link

--- projects/console-web-front/src/components/common/ExternalGuideLink.tsx
+++ projects/console-web-front/src/components/common/ExternalGuideLink.tsx
@@ -6,7 +6,7 @@ interface Props extends Omit<ButtonProps, 'type' | 'target'> {}
 
 const ExternalGuideLink = ({ children, ...props }: Props) => {
   return (
-    <StyledButton type="link" target="_blank" {...props}>
+    <StyledButton target="_blank" {...props}>
       <p style={{ marginLeft: '.5rem' }}>{children}</p>
       &nbsp;
       <BiLinkExternal />
@@ -17,7 +17,6 @@ const ExternalGuideLink = ({ children, ...props }: Props) => {
 export default ExternalGuideLink;
 
 const StyledButton = styled(Button)`
-  color: ${(props) => props.theme.colorPrimary};
   display: flex;
   flex-direction: row;
   align-items: center;
