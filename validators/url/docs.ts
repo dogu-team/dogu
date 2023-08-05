5678731ad7ad9fb6f6384be041069395f994fec0
Daniel Ahn <daniel@dogutech.io>
2023-08-05 18:01:34 +0900

    ci: docs exclude url

--- validators/url/docs.ts
+++ validators/url/docs.ts
@@ -1,7 +1,11 @@
 import path from 'path';
 import { validate } from './validation';
 
-const exceptionUrls: string[] = ['https://DOGU_HOST:3001'];
+const exceptionUrls: string[] = [
+  'https://DOGU_HOST:3001',
+  'https://api.dogutech.io/v1/projects/${projectId}/applications',
+  'https://api.dogutech.io/v1/projects/${projectId}/routines/${routineId}/pipelines',
+];
 const checkedUrls: { [url: string]: boolean } = {};
 const docsPath = path.join(__dirname, '../../docs');
 const targetPaths = [`${docsPath}/docs`, `${docsPath}/src`, `${docsPath}/i18n`];
