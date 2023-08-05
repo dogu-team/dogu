5c01eefc05ea5062c05bab343936ae061723bda7
Daniel Ahn <daniel@dogutech.io>
2023-08-05 12:45:08 +0900

    fix: integration guide link style

--- projects/console-web-front/src/components/pipelines/GithubActionButton.tsx
+++ projects/console-web-front/src/components/pipelines/GithubActionButton.tsx
@@ -1,6 +1,8 @@
 import { RoutineBase } from '@dogu-private/console';
 import { Button } from 'antd';
 import Image from 'next/image';
+import { BiLinkExternal } from 'react-icons/bi';
+import styled from 'styled-components';
 
 interface Props {
   routine?: RoutineBase;
@@ -12,21 +14,30 @@ const GithubActionButton = ({ routine }: Props) => {
   }
 
   return (
-    <Button
+    <StyledButton
       href={'https://docs.dogutech.io/integration/cicd/github'}
       target="_blank"
-      style={{
-        display: 'flex',
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center',
-        marginLeft: '.5rem',
-      }}
+      type="link"
       icon={<Image src="/resources/icons/github-action-logo.svg" alt="Github Action" width={16} height={16} />}
     >
-      <p style={{ marginLeft: '8px' }}>Github Action</p>
-    </Button>
+      <p style={{ marginLeft: '8px' }}>Github Action</p>&nbsp;
+      <BiLinkExternal />
+    </StyledButton>
   );
 };
 
 export default GithubActionButton;
+
+const StyledButton = styled(Button)`
+  color: ${(props) => props.theme.colorPrimary};
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  margin-left: 0.5rem;
+  padding: 4px 8px;
+
+  &:active {
+    color: ${(props) => props.theme.colorPrimary} !important;
+  }
+`;
--- projects/console-web-front/src/components/pipelines/JenkinsButton.tsx
+++ projects/console-web-front/src/components/pipelines/JenkinsButton.tsx
@@ -1,6 +1,8 @@
 import { RoutineBase } from '@dogu-private/console';
 import { Button } from 'antd';
 import Image from 'next/image';
+import styled from 'styled-components';
+import { BiLinkExternal } from 'react-icons/bi';
 
 interface Props {
   routine?: RoutineBase;
@@ -12,21 +14,30 @@ const JenkinsButton = ({ routine }: Props) => {
   }
 
   return (
-    <Button
+    <StyledButton
       href={'https://docs.dogutech.io/integration/cicd/jenkins'}
       target="_blank"
-      style={{
-        display: 'flex',
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'center',
-        marginLeft: '.5rem',
-      }}
+      type="link"
       icon={<Image src="/resources/icons/jenkins-logo.svg" alt="Jenkins" width={16} height={16} />}
     >
-      <p style={{ marginLeft: '8px' }}>Jenkins</p>
-    </Button>
+      <p style={{ marginLeft: '8px' }}>Jenkins</p>&nbsp;
+      <BiLinkExternal />
+    </StyledButton>
   );
 };
 
 export default JenkinsButton;
+
+const StyledButton = styled(Button)`
+  color: ${(props) => props.theme.colorPrimary};
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  margin-left: 0.5rem;
+  padding: 4px 8px;
+
+  &:active {
+    color: ${(props) => props.theme.colorPrimary} !important;
+  }
+`;
