8859bd9c4afbe8df906331730f4b0f5d626006fb
Daniel Ahn <daniel@dogutech.io>
2023-08-09 17:15:59 +0900

    feat: parse & utilize class for page source

---
+++ packages/typescript-private/console/src/util/page-source/gamium.ts
@@ -0,0 +1,174 @@
+import { plainToInstance } from 'class-transformer';
+
+import {
+  ConvertElementToNodeFunc,
+  DeviceRotationDirection,
+  GamiumNode,
+  GamiumNodeAttributeFields,
+  GamiumNodeAttributes,
+  GamiumRotation,
+  GetDeviceRotationFunc,
+  GetDeviceScreenSizeFunc,
+  GetInspectingAreaFunc,
+  GetNodeBoundFunc,
+  NodeUtilizer,
+  PageSourceParser,
+  ParseToNodeFunc,
+} from './types';
+
+export class GamiumPageSourceParser extends PageSourceParser<GamiumNodeAttributes> {
+  public convertElementToNode: ConvertElementToNodeFunc<GamiumNodeAttributes> = (element, parentNode) => {
+    const json: GamiumNode = {
+      tag: element.nodeName,
+      key: '',
+      title: '',
+      attributes: {
+        index: 1,
+      },
+    };
+
+    const rawAttributes: { [key in GamiumNodeAttributeFields]?: GamiumNodeAttributes[key] } = {};
+
+    // Convert element attributes to JSON properties
+    for (let i = 0; i < element.attributes.length; i++) {
+      const attr = element.attributes[i];
+      const attrName = attr.name as GamiumNodeAttributeFields;
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      rawAttributes[attrName] = attr.value;
+    }
+
+    json.attributes = plainToInstance(GamiumNodeAttributes, rawAttributes);
+
+    const name = json.attributes['name'];
+
+    let key = parentNode?.key || '';
+    let path = parentNode?.attributes.path || '';
+    if (element.tagName !== 'hierarchy') {
+      const currentKey = `${name || element['tagName']}[${rawAttributes.index ?? 1}]`;
+
+      if (key === '/') {
+        key = `${key}${currentKey}`;
+      } else {
+        key = `${key}/${currentKey}`;
+      }
+
+      // gamium xpath rule. scenes are root
+      const currentPath = `${name || element['tagName']}[${rawAttributes.index ?? 1}]`;
+      if ((element.parentNode as HTMLElement).tagName === 'hierarchy') {
+        path = '/';
+      } else {
+        if (path === '/') {
+          path = `${path}${currentPath}`;
+        } else {
+          path = `${path}/${currentPath}`;
+        }
+      }
+    } else {
+      key = '/';
+      path = '';
+    }
+
+    json.key = key;
+    json.attributes.path = path;
+    json.title = `${name || element['tagName']}` || 'No title';
+
+    // Convert child elements recursively
+    if (element.childNodes.length > 0) {
+      json.children = [];
+      for (let i = 0; i < element.childNodes.length; i++) {
+        const child = element.childNodes[i];
+
+        // Ignore empty text nodes
+        if (child.nodeValue) {
+          continue;
+        }
+
+        json.children.push(this.convertElementToNode(child as HTMLElement, json));
+      }
+    }
+
+    return json;
+  };
+
+  public parseToNode: ParseToNodeFunc<GamiumNodeAttributes> = () => {
+    const result = this.convertElementToNode(this.rootElement);
+    return result;
+  };
+}
+
+export class GamiumNodeUtilizer extends NodeUtilizer<GamiumNodeAttributes> {
+  public getNodeBound: GetNodeBoundFunc<GamiumNodeAttributes> = (node) => {
+    const screenPosition = node.attributes['screen-position'];
+    const screenRectSize = node.attributes['screen-rect-size'];
+    const deviceScreenSize = this.getDeviceScreenSize();
+
+    if (!screenPosition) {
+      return {
+        x: 0,
+        y: 0,
+        width: screenRectSize?.width || 0,
+        height: screenRectSize?.height || 0,
+      };
+    }
+
+    if (!screenRectSize) {
+      return {
+        x: screenPosition.x,
+        y: screenPosition.y,
+        width: 0,
+        height: 0,
+      };
+    }
+
+    /*
+     * screenPosition is cetner of screenRectSize
+     * screenPosition is start (0,0) from bottom left of screen
+     * need to convert to start (0,0) from top left of screen
+     */
+    return {
+      x: screenPosition.x - screenRectSize.width / 2,
+      y: deviceScreenSize.height - screenPosition.y - screenRectSize.height / 2,
+      width: screenRectSize.width,
+      height: screenRectSize.height,
+    };
+  };
+
+  public getInspectingArea: GetInspectingAreaFunc = () => {
+    const deviceSize = this.getDeviceScreenSize();
+
+    return {
+      x: 0,
+      y: 0,
+      ...deviceSize,
+    };
+  };
+
+  public getDeviceRotation: GetDeviceRotationFunc = () => {
+    const orientation = this.contextAndNode.node.attributes.orientation;
+
+    switch (orientation) {
+      case GamiumRotation.PORTRAIT:
+        return DeviceRotationDirection.TOP_DOWN;
+      case GamiumRotation.LANDSCAPE_LEFT:
+        return DeviceRotationDirection.LEFT;
+      case GamiumRotation.LANDSCAPE_RIGHT:
+        return DeviceRotationDirection.RIGHT;
+      case GamiumRotation.PORTRAIT_UPSIDE_DOWN:
+        return DeviceRotationDirection.UPSIDE_DOWN;
+      case GamiumRotation.AUTO_ROTATION:
+      default:
+        return DeviceRotationDirection.TOP_DOWN;
+    }
+  };
+
+  public getDeviceScreenSize: GetDeviceScreenSizeFunc = () => {
+    const width = this.contextAndNode.node.attributes.width;
+    const height = this.contextAndNode.node.attributes.height;
+
+    return {
+      width: width || this.contextAndNode.screenSize.width,
+      height: height || this.contextAndNode.screenSize.height,
+    };
+  };
+}
---
+++ packages/typescript-private/console/src/util/page-source/index.ts
@@ -0,0 +1,105 @@
+import { Platform } from '@dogu-private/types';
+import { DOMParser } from 'xmldom';
+
+import { AndroidNodeUtilizer, AndroidPageSourceParser } from './android';
+import { GamiumNodeUtilizer, GamiumPageSourceParser } from './gamium';
+import { IosNodeUtilizer, IosPageSourceParser } from './ios';
+import { AndroidNode, AndroidNodeAttributes, ContextNode, GamiumNode, GamiumNodeAttributes, IosNode, IosNodeAttributes, NodeAttributes, ParsedNode } from './types';
+
+function webviewToElement<A>(element: HTMLElement, parentKey: string, index: number): ParsedNode<A> {
+  const json: ParsedNode<A> = {
+    tag: element.nodeName,
+    key: '',
+    title: '',
+    attributes: {} as A,
+  };
+
+  const rawAttributes: { [key in keyof A]?: string } = {};
+
+  // Convert element attributes to JSON properties
+  for (let i = 0; i < element.attributes.length; i++) {
+    const attr = element.attributes[i];
+    const attrName = attr.name as keyof A;
+    rawAttributes[attrName] = attr.value;
+  }
+
+  // json.attributes = plainToInstance({}, rawAttributes);
+  json.attributes = rawAttributes as A;
+
+  let key = parentKey;
+  if (element.tagName !== 'hierarchy') {
+    const currentPath = `${element.tagName}[${index}]`;
+    if (parentKey === '/') {
+      key = `${parentKey}${currentPath}`;
+    } else {
+      key = `${parentKey}/${currentPath}`;
+    }
+  } else {
+    key = '/';
+  }
+  json.key = key;
+  json.title = element.tagName;
+
+  // Convert child elements recursively
+  if (element.childNodes.length > 0) {
+    json.children = [];
+    for (let i = 0; i < element.childNodes.length; i++) {
+      const child = element.childNodes[i];
+
+      // Ignore empty text nodes
+      if (child.nodeValue) {
+        continue;
+      }
+
+      json.children.push(webviewToElement(child as HTMLElement, key, i));
+    }
+  }
+
+  return json;
+}
+
+export class PageSourceParserFacade {
+  public parse(platform: Platform, pageSource: string): GamiumNode | AndroidNode | IosNode {
+    const removeSpace = pageSource.replace(/(\r|\n)( ?)+/g, '');
+    const parser = new DOMParser();
+    const xmlDoc = parser.parseFromString(removeSpace, 'text/xml');
+    const root = xmlDoc.documentElement;
+    const isGamiumElement = root.attributes.getNamedItem('gamium-version') !== undefined;
+    const isWebview = root.tagName === 'html';
+
+    if (isGamiumElement) {
+      return new GamiumPageSourceParser(root).parseToNode();
+    }
+
+    if (isWebview) {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      return webviewToElement<any>(root, '', 0);
+    }
+
+    switch (platform) {
+      case Platform.PLATFORM_ANDROID:
+        return new AndroidPageSourceParser(root).parseToNode();
+      case Platform.PLATFORM_IOS:
+        return new IosPageSourceParser(root).parseToNode();
+      default:
+        throw new Error(`Unsupported platform: ${platform}`);
+    }
+  }
+}
+
+export class NodeUtilizerFactory {
+  static create(platform: Platform, contextAndNode: ContextNode<NodeAttributes>): GamiumNodeUtilizer | AndroidNodeUtilizer | IosNodeUtilizer {
+    if (contextAndNode.context === 'GAMIUM') {
+      return new GamiumNodeUtilizer(contextAndNode as ContextNode<GamiumNodeAttributes>);
+    }
+
+    switch (platform) {
+      case Platform.PLATFORM_ANDROID:
+        return new AndroidNodeUtilizer(contextAndNode as ContextNode<AndroidNodeAttributes>);
+      case Platform.PLATFORM_IOS:
+        return new IosNodeUtilizer(contextAndNode as ContextNode<IosNodeAttributes>);
+      default:
+        throw new Error(`Unsupported platform: ${platform}`);
+    }
+  }
+}
