78d9903f6a771b96bf4c8572b8c4a21ebd2d0e8d
Daniel Ahn <daniel@dogutech.io>
2023-08-17 18:48:41 +0900

    feat: select input text when copy failed

--- projects/console-web-front/src/components/common/TokenCopyInput.tsx
+++ projects/console-web-front/src/components/common/TokenCopyInput.tsx
@@ -1,6 +1,7 @@
 import { CopyOutlined } from '@ant-design/icons';
-import { Input, message } from 'antd';
+import { Input, InputRef, message } from 'antd';
 import useTranslation from 'next-translate/useTranslation';
+import { useRef } from 'react';
 import styled from 'styled-components';
 
 interface Props {
@@ -9,18 +10,28 @@ interface Props {
 
 const TokenCopyInput = ({ value }: Props) => {
   const { t } = useTranslation();
+  const inputRef = useRef<InputRef>(null);
 
   const handleCopy = async () => {
     try {
+      throw new Error('!');
       await navigator.clipboard.writeText(value);
       message.success(t('common:copyClipboard'));
     } catch (e) {
       message.error(t('common:copyClipboardFailed'));
+      inputRef.current?.focus();
+      inputRef.current?.setSelectionRange(0, value.length);
     }
   };
 
   return (
-    <StyledInput access-id={process.env.NEXT_PUBLIC_ENV !== 'production' ? 'copy-token-input' : undefined} value={value} onSearch={handleCopy} enterButton={<CopyOutlined />} />
+    <StyledInput
+      ref={inputRef}
+      access-id={process.env.NEXT_PUBLIC_ENV !== 'production' ? 'copy-token-input' : undefined}
+      value={value}
+      onSearch={handleCopy}
+      enterButton={<CopyOutlined />}
+    />
   );
 };
 
