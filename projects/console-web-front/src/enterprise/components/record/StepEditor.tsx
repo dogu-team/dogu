f4c4695bae70e6c31cd3d2a9af72b4c9d5b9b2a6
Daniel Ahn <daniel@dogutech.io>
2023-08-18 20:37:36 +0900

    feat: image size with real size ratio

--- projects/console-web-front/src/enterprise/components/record/StepEditor.tsx
+++ projects/console-web-front/src/enterprise/components/record/StepEditor.tsx
@@ -1,5 +1,6 @@
 import { RecordTestStepBase } from '@dogu-private/console';
 import Image from 'next/image';
+import { useRef } from 'react';
 import styled from 'styled-components';
 import { flexRowCenteredStyle } from '../../../styles/box';
 import DeleteStepButton from './DeleteStepButton';
@@ -9,13 +10,46 @@ interface Props {
 }
 
 const StepEditor = ({ step }: Props) => {
+  const itemRef = useRef<HTMLDivElement>(null);
+  const imageWrapperRef = useRef<HTMLDivElement>(null);
+
+  const resizeImageAndAddBound = (img: HTMLImageElement) => {
+    if (itemRef.current && imageWrapperRef.current) {
+      const parentWidth = itemRef.current.clientWidth;
+      const parentHeight = itemRef.current.clientHeight;
+      const imageWidth = img.naturalWidth;
+      const imageHeight = img.naturalHeight;
+      const imageRatio = imageWidth / imageHeight;
+
+      imageWrapperRef.current.style.maxHeight = `${parentHeight}px`;
+
+      if (imageRatio > 1) {
+        imageWrapperRef.current.style.width = `100%`;
+        imageWrapperRef.current.style.height = `${(imageHeight * parentWidth) / imageWidth}px`;
+      } else {
+        imageWrapperRef.current.style.width = `${(imageWidth * parentHeight) / imageHeight}px`;
+        imageWrapperRef.current.style.height = '100%';
+      }
+    }
+  };
+
   return (
     <Box>
       {step ? (
         <FlexRow>
-          <RelativeBox>
-            <Image src={step.screenshotUrl} fill sizes="(max-width: 767px) 100vw, 33vw" quality={90} style={{ objectFit: 'contain' }} alt={step.recordTestStepId} />
-          </RelativeBox>
+          <FlexItem ref={itemRef}>
+            <RelativeBox ref={imageWrapperRef}>
+              <Image
+                src={step.screenshotUrl}
+                fill
+                sizes="(max-width: 767px) 100vw, 33vw"
+                quality={90}
+                style={{ objectFit: 'contain' }}
+                alt={step.recordTestStepId}
+                onLoadingComplete={resizeImageAndAddBound}
+              />
+            </RelativeBox>
+          </FlexItem>
           <MenuWrapper>
             <DeleteStepButton step={step} />
           </MenuWrapper>
@@ -44,7 +78,12 @@ const MenuWrapper = styled.div`
   margin-left: 0.5rem;
 `;
 
+const FlexItem = styled.div`
+  display: flex;
+  justify-content: center;
+  flex: 1;
+`;
+
 const RelativeBox = styled.div`
   position: relative;
-  flex: 1;
 `;
