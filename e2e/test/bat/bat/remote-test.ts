a640de8f301fc8b12e329b43fa8785232b83d1fb
oneofthezombies <hunhoekim@gmail.com>
2023-08-19 12:12:01 +0900

    chore: add checkout path

--- e2e/test/bat/bat/remote-test.ts
+++ e2e/test/bat/bat/remote-test.ts
@@ -1,21 +1,5 @@
-import { job } from '@dogu-tech/dest';
+import { job, test } from '@dogu-tech/dest';
 import { PlaywrightDriver } from '../../../src/playwright-driver';
-
-// async function spawnExec(command: string): Promise<void> {
-//   await new Promise((resolve, reject) => {
-//     const child = spawn(command, { shell: true });
-//     const onErrorForReject = (error: Error) => {
-//       reject(error);
-//     };
-//     child.on('error', onErrorForReject);
-//     child.on('spawn', () => {
-//       child.off('error', onErrorForReject);
-//       child.on('error', (error: Error) => {
-//         console.error(`Error on command: ${command}`);
-//       });
-//     });
-//   });
-// }
 
 export interface TestRemoteOptions {
   consoleFrontDriver: PlaywrightDriver;
