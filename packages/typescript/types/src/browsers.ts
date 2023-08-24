097a14c900e10ab2f54966ea983c98bed9d021d4
oneofthezombies <hunhoekim@gmail.com>
2023-08-24 14:44:27 +0900

    wip: add browser types

--- packages/typescript/types/src/browsers.ts
+++ packages/typescript/types/src/browsers.ts
@@ -1,15 +1,35 @@
-export const BrowserName = ['chrome', 'chromium', 'firefox', 'safari', 'webkit', 'edge', 'ie'] as const;
+import { PlatformType } from './platforms';
+
+export const BrowserName = ['chrome', 'firefox', 'safari', 'safaritp', 'edge', 'iexplorer', 'samsung-internet'] as const;
 export type BrowserName = (typeof BrowserName)[number];
+export const isAllowedBrowserName = (value: string): value is BrowserName => BrowserName.includes(value as BrowserName);
 
-export function isValidBrowserName(value: string): value is BrowserName {
-  return BrowserName.includes(value as BrowserName);
-}
+export const DriverName = ['chromedriver', 'geckodriver', 'safaridriver', 'msedgedriver', 'IEDriverServer'] as const;
+export type DriverName = (typeof DriverName)[number];
+export const isAllowedDriverName = (value: string): value is DriverName => DriverName.includes(value as DriverName);
 
-export const BrowserDriverName = ['chromedriver', 'geckodriver', 'safaridriver'] as const;
-export type BrowserDriverName = (typeof BrowserDriverName)[number];
+export type BrowserOrDriverName = BrowserName | DriverName;
 
-export function isValidBrowserDriverName(value: string): value is BrowserDriverName {
-  return BrowserDriverName.includes(value as BrowserDriverName);
-}
+export const BrowserPlatform = ['macos', 'windows', 'android', 'ios'] as const;
+export type BrowserPlatform = (typeof BrowserPlatform)[number] extends Extract<PlatformType, 'macos' | 'windows' | 'android' | 'ios'> ? (typeof BrowserPlatform)[number] : never;
 
-export type BrowserOrDriverName = BrowserName | BrowserDriverName;
+export type MacosBrowserName = Extract<BrowserName, 'chrome' | 'firefox' | 'safari' | 'safaritp' | 'edge'>;
+export type WindowsBrowserName = Extract<BrowserName, 'chrome' | 'firefox' | 'edge' | 'iexplorer'>;
+
+export type AndroidBrowserName = Extract<BrowserName, 'chrome' | 'firefox' | 'edge' | 'samsung-internet'>;
+export const AndroidBrowserAppIdMap: Record<AndroidBrowserName, string> = {
+  chrome: 'com.android.chrome',
+  firefox: 'org.mozilla.firefox',
+  edge: 'com.microsoft.emmx',
+  'samsung-internet': 'com.sec.android.app.sbrowser',
+} as const;
+export type AndroidBrowserAppId = keyof typeof AndroidBrowserAppIdMap;
+
+export type IosBrowserName = Extract<BrowserName, 'chrome' | 'firefox' | 'safari' | 'edge'>;
+export const IosBrowserAppIdMap: Record<IosBrowserName, string> = {
+  chrome: 'com.google.chrome.ios',
+  firefox: 'org.mozilla.iosfirefox',
+  edge: 'com.microsoft.edge.ios',
+  safari: 'com.apple.mobilesafari',
+} as const;
+export type IosBrowserAppId = keyof typeof IosBrowserAppIdMap;
