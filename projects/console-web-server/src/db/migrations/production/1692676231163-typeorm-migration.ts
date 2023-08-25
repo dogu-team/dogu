ba94d1c2d6577616d5f7c3e028f5ca7a8d1c481c
Byoungwook Park <qkrquddnr99@gmail.com>
2023-08-25 19:53:17 +0900

    fix: update db migration error

--- projects/console-web-server/src/db/migrations/production/1692676231163-typeorm-migration.ts
+++ projects/console-web-server/src/db/migrations/production/1692676231163-typeorm-migration.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class typeormMigration1692676231162 implements MigrationInterface {
-  name = 'typeormMigration1692676231162';
+export class typeormMigration1692676231163 implements MigrationInterface {
+  name = 'typeormMigration1692676231163';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`UPDATE "device" SET "serial_unique" = "serial"`);
