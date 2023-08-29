0e411904b13d7681e33308737352e5cec360efca
oneofthezombies <hunhoekim@gmail.com>
2023-08-29 22:48:47 +0900

    wip: remove local user project flag

--- dogu-actions/prepare/src/main.ts
+++ dogu-actions/prepare/src/main.ts
@@ -1,9 +1,9 @@
-import { ActionKit, AppVersion, checkoutProject, downloadApp, errorify, isAppVersion, OptionsConfig, stringify } from '@dogu-tech/action-kit';
+import { ActionKit, AppVersion, checkoutProject, downloadApp, errorify, isAppVersion, stringify } from '@dogu-tech/action-kit';
 import { tryToQuitGamiumApp } from '@dogu-tech/toolkit';
 import path from 'path';
 
 ActionKit.run(async ({ options, logger, input, deviceHostClient, consoleActionClient, deviceClient }) => {
-  const { DOGU_ROUTINE_WORKSPACE_PATH, DOGU_DEVICE_PLATFORM, DOGU_HOST_WORKSPACE_PATH, DOGU_DEVICE_SERIAL, DOGU_RUN_TYPE } = options;
+  const { DOGU_ROUTINE_WORKSPACE_PATH, DOGU_DEVICE_PLATFORM, DOGU_HOST_WORKSPACE_PATH, DOGU_DEVICE_SERIAL } = options;
   const clean = input.get<boolean>('clean');
   const appVersion = input.get<AppVersion>('appVersion');
   if (!isAppVersion(appVersion)) {
@@ -32,12 +32,8 @@ ActionKit.run(async ({ options, logger, input, deviceHostClient, consoleActionCl
   const resolvedCheckoutPath = path.resolve(DOGU_ROUTINE_WORKSPACE_PATH, checkoutPath);
   logger.info('resolved checkout path', { resolvedCheckoutPath });
 
-  const optionsConfig = await OptionsConfig.load();
-  if (optionsConfig.get('localUserProject.use', false)) {
-    logger.info('Using local user project...');
-  } else {
-    await checkoutProject(logger, consoleActionClient, deviceHostClient, resolvedCheckoutPath, branchOrTag, clean, checkoutUrl);
-  }
+  await checkoutProject(logger, consoleActionClient, deviceHostClient, resolvedCheckoutPath, branchOrTag, clean, checkoutUrl);
+
   const appPath = await downloadApp(logger, consoleActionClient, deviceHostClient, DOGU_DEVICE_PLATFORM, DOGU_HOST_WORKSPACE_PATH, currentPlatformAppVersion);
   await tryToQuitGamiumApp(logger, deviceClient, deviceHostClient, gamiumEnginePort, DOGU_DEVICE_SERIAL, DOGU_DEVICE_PLATFORM, retryCount, retryInterval, requestTimeout);
   if (uninstallApp) {
