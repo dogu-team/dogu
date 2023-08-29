104856098b739b280f4b4e6617ef9caaa8884d28
dev-kyun <84701844+dev-kyun@users.noreply.github.com>
2023-08-29 14:35:10 +0900

    update: record-test-scenario crud (#370)

--- packages/typescript-private/console/src/dto/record/record-test-scenario.dto.ts
+++ packages/typescript-private/console/src/dto/record/record-test-scenario.dto.ts
@@ -1,15 +1,20 @@
+import { RecordTestCaseId } from '@dogu-private/types';
 import { RecordTestCaseResponse, RecordTestScenarioAndRecordTestCaseBase, RecordTestScenarioBase } from '../../index';
 import { PageDtoBase } from '../pagination/page.dto';
 
 export interface CreateRecordTestScenarioDtoBase extends Pick<RecordTestScenarioBase, 'name'> {
   name: string;
+  recordTestCaseIds?: RecordTestCaseId[];
 }
 
 export interface FindRecordTestScenariosByProjectIdDtoBase extends PageDtoBase {
   keyword?: string;
 }
 
-export type UpdateRecordTestScenarioDtoBase = Pick<RecordTestScenarioBase, 'name'>;
+export interface UpdateRecordTestScenarioDtoBase extends Pick<RecordTestScenarioBase, 'name'> {
+  name: string;
+  recordTestCaseIds?: RecordTestCaseId[];
+}
 
 export interface RecordTestScenarioResponse extends RecordTestScenarioBase {
   recordTestCases: RecordTestCaseResponse[];
