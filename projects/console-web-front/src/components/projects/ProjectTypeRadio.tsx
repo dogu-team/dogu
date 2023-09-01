84ee5c3f90ccea9b67825ba303972be463e99ae2
Daniel Ahn <daniel@dogutech.io>
2023-09-01 15:38:26 +0900

    feat: description for template (#348)

--- projects/console-web-front/src/components/projects/ProjectTypeRadio.tsx
+++ projects/console-web-front/src/components/projects/ProjectTypeRadio.tsx
@@ -10,42 +10,64 @@ const ProjectTypeRadio = (props: Omit<RadioGroupProps, 'children'>) => {
     fontSize: '1.75rem',
   };
 
+  const getDescriptionText = (type: PROJECT_TYPE) => {
+    switch (type) {
+      case PROJECT_TYPE.WEB:
+        return '* Specialized for web(includes mobile) application.';
+      case PROJECT_TYPE.APP:
+        return '* Specialized for Android and iOS mobile app.';
+      case PROJECT_TYPE.GAME:
+        return '* Specialized for mobile and desktop game.';
+      case PROJECT_TYPE.CUSTOM:
+        return '* Able to customize project features for your needs.';
+      default:
+        return '';
+    }
+  };
+
   return (
-    <StyledRadioGroup defaultValue={PROJECT_TYPE.WEB} buttonStyle="solid" {...props}>
-      <StyledRadio value={PROJECT_TYPE.WEB}>
-        <IconWrapper>
-          <ProjectTypeIcon type={PROJECT_TYPE.WEB} style={iconStyle} />
-        </IconWrapper>
-        <Title>Web</Title>
-      </StyledRadio>
-      <StyledRadio value={PROJECT_TYPE.APP}>
-        <IconWrapper>
-          <ProjectTypeIcon type={PROJECT_TYPE.APP} style={iconStyle} />
-        </IconWrapper>
-        <Title>Mobile App</Title>
-      </StyledRadio>
-      <StyledRadio value={PROJECT_TYPE.GAME}>
-        <IconWrapper>
-          <ProjectTypeIcon type={PROJECT_TYPE.GAME} style={iconStyle} />
-        </IconWrapper>
-        <Title>Game</Title>
-      </StyledRadio>
-      <StyledRadio value={PROJECT_TYPE.CUSTOM}>
-        <IconWrapper>
-          <ProjectTypeIcon type={PROJECT_TYPE.CUSTOM} style={iconStyle} />
-        </IconWrapper>
-        <Title>Custom</Title>
-      </StyledRadio>
-    </StyledRadioGroup>
+    <Box>
+      <StyledRadioGroup defaultValue={PROJECT_TYPE.WEB} buttonStyle="solid" {...props}>
+        <StyledRadio value={PROJECT_TYPE.WEB}>
+          <IconWrapper>
+            <ProjectTypeIcon type={PROJECT_TYPE.WEB} style={iconStyle} />
+          </IconWrapper>
+          <Title>Web</Title>
+        </StyledRadio>
+        <StyledRadio value={PROJECT_TYPE.APP}>
+          <IconWrapper>
+            <ProjectTypeIcon type={PROJECT_TYPE.APP} style={iconStyle} />
+          </IconWrapper>
+          <Title>Mobile App</Title>
+        </StyledRadio>
+        <StyledRadio value={PROJECT_TYPE.GAME}>
+          <IconWrapper>
+            <ProjectTypeIcon type={PROJECT_TYPE.GAME} style={iconStyle} />
+          </IconWrapper>
+          <Title>Game</Title>
+        </StyledRadio>
+        <StyledRadio value={PROJECT_TYPE.CUSTOM}>
+          <IconWrapper>
+            <ProjectTypeIcon type={PROJECT_TYPE.CUSTOM} style={iconStyle} />
+          </IconWrapper>
+          <Title>Custom</Title>
+        </StyledRadio>
+      </StyledRadioGroup>
+
+      <Description>{getDescriptionText(props.value as PROJECT_TYPE)}</Description>
+    </Box>
   );
 };
 
 export default ProjectTypeRadio;
 
+const Box = styled.div``;
+
 const StyledRadioGroup = styled(Radio.Group)`
   width: 100%;
   display: flex;
   align-items: center;
+  margin-bottom: 0.5rem;
 `;
 
 const StyledRadio = styled(Radio.Button)`
@@ -67,4 +89,9 @@ const Title = styled.b`
   margin-top: 0.25rem;
   display: block;
   line-height: 1.5;
+`;
+
+const Description = styled.p`
+  font-size: 0.9rem;
+  color: ${(props) => props.theme.main.colors.gray2};
 `;
