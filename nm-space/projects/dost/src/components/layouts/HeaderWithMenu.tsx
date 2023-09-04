d0eb2d9927727c14270bbde5fdf07db59b3385ef
oneofthezombies <hunhoekim@gmail.com>
2023-09-04 22:00:18 +0900

    fix: add report zip button to headers

--- nm-space/projects/dost/src/components/layouts/HeaderWithMenu.tsx
+++ nm-space/projects/dost/src/components/layouts/HeaderWithMenu.tsx
@@ -1,33 +1,42 @@
-import { ColorMode, Stack } from '@chakra-ui/react';
+import { ColorMode, Stack, useDisclosure, Tooltip } from '@chakra-ui/react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
-import { AiFillSetting } from 'react-icons/ai';
+import { AiFillSetting, AiFillFileZip } from 'react-icons/ai';
 import { IoMdHelpCircle } from 'react-icons/io';
 
 import { ipc } from '../../utils/window';
 import DevButton from '../DevButton';
 import Header from './Header';
 import HeaderIconMenuButon from './HeaderIconMenuButon';
+import { ReportLogAlert } from '../overlays/ReportLogAlert';
 
 const HeaderWithMenu = () => {
   const location = useLocation();
   const navigate = useNavigate();
+  const { isOpen: isReportOpen, onOpen: onReportOpen, onClose: onReportClose } = useDisclosure();
 
   const openDocs = async () => {
     await ipc.settingsClient.openExternal('https://docs.dogutech.io');
   };
 
   return (
-    <Header
-      right={
-        <Stack direction="row" spacing="8px" alignItems={'center'}>
-          <HeaderIconMenuButon icon={<IoMdHelpCircle style={{ fontSize: '18px' }} />} onClick={openDocs} />
-          <HeaderIconMenuButon icon={<AiFillSetting style={{ fontSize: '18px' }} />} onClick={() => navigate('/settings')} selected={location.pathname === '/settings'} />
+    <>
+      <Header
+        right={
+          <Stack direction="row" spacing="8px" alignItems={'center'}>
+            <HeaderIconMenuButon icon={<IoMdHelpCircle style={{ fontSize: '18px' }} />} onClick={openDocs} />
 
-          <DevButton />
-        </Stack>
-      }
-    />
+            <Tooltip label="Create Report Zip" placement="bottom" shouldWrapChildren>
+              <HeaderIconMenuButon icon={<AiFillFileZip style={{ fontSize: '18px' }} />} onClick={onReportOpen} />
+            </Tooltip>
+
+            <HeaderIconMenuButon icon={<AiFillSetting style={{ fontSize: '18px' }} />} onClick={() => navigate('/settings')} selected={location.pathname === '/settings'} />
+            <DevButton />
+          </Stack>
+        }
+      />
+      <ReportLogAlert isOpen={isReportOpen} onClose={onReportClose} />
+    </>
   );
 };
 
--- nm-space/projects/dost/src/components/layouts/SetupLayout.tsx
+++ nm-space/projects/dost/src/components/layouts/SetupLayout.tsx
@@ -1,33 +1,45 @@
-import { Stack } from '@chakra-ui/react';
+import { Stack, Tooltip, useDisclosure } from '@chakra-ui/react';
+import { AiFillFileZip } from 'react-icons/ai';
 import { IoMdHelpCircle } from 'react-icons/io';
 import { Outlet } from 'react-router-dom';
 
 import { ipc } from '../../utils/window';
 import DevButton from '../DevButton';
+import { ReportLogAlert } from '../overlays/ReportLogAlert';
 import BasicLayout from './BasicLayout';
 import Header from './Header';
 import HeaderIconMenuButon from './HeaderIconMenuButon';
 
 const SetupLayout = () => {
+  const { isOpen: isReportOpen, onOpen: onReportOpen, onClose: onReportClose } = useDisclosure();
+
   const openDocs = async () => {
     await ipc.settingsClient.openExternal('https://docs.dogutech.io');
   };
 
   return (
-    <BasicLayout
-      header={
-        <Header
-          right={
-            <Stack direction="row" spacing="8px" alignItems={'center'}>
-              <HeaderIconMenuButon icon={<IoMdHelpCircle style={{ fontSize: '18px' }} />} onClick={openDocs} />
-              <DevButton />
-            </Stack>
-          }
-        />
-      }
-    >
-      <Outlet />
-    </BasicLayout>
+    <>
+      <BasicLayout
+        header={
+          <Header
+            right={
+              <Stack direction="row" spacing="8px" alignItems={'center'}>
+                <HeaderIconMenuButon icon={<IoMdHelpCircle style={{ fontSize: '18px' }} />} onClick={openDocs} />
+
+                <Tooltip label="Create Report Zip" placement="bottom" shouldWrapChildren>
+                  <HeaderIconMenuButon icon={<AiFillFileZip style={{ fontSize: '18px' }} />} onClick={onReportOpen} />
+                </Tooltip>
+
+                <DevButton />
+              </Stack>
+            }
+          />
+        }
+      >
+        <Outlet />
+      </BasicLayout>
+      <ReportLogAlert isOpen={isReportOpen} onClose={onReportClose} />
+    </>
   );
 };
 
