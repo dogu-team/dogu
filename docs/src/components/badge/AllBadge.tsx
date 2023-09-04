c1a35f3d2361dbf2e1a1878746df6fd7f8e0573a
Seong JuWon <lumyjuwon@gmail.com>
2023-09-04 21:28:57 +0900

    docs: update badges

--- docs/src/components/badge/AllBadge.tsx
+++ docs/src/components/badge/AllBadge.tsx
@@ -6,7 +6,7 @@ const AllBadge = () => {
     <Badge
       label={'ALL Support'}
       overlayText={
-        'Available in the cloud, self-hosted community, self-hosted professional'
+        'Available in the cloud free, cloud professional, self-hosted community, self-hosted professional'
       }
       fontColor={'white'}
       backgroundColor={'#ffc655'}
---
+++ docs/src/components/badge/SelfHostedProfessionalBadge.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Badge from './Badge';
+
+const SelfHostedProfessionalBadge = () => {
+  return (
+    <Badge
+      label={'Self-Hosted Professional'}
+      overlayText={'Available in the self-hosted professional'}
+      backgroundColor={'#1bc967'}
+    ></Badge>
+  );
+};
+
+export default SelfHostedProfessionalBadge;
