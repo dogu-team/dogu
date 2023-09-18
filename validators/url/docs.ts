e218ee154db7def485324d0423df49f1912c7a53
Daniel Ahn <daniel@dogutech.io>
2023-09-18 22:15:47 +0900

    docs: whitelist for validation url

--- validators/url/docs.ts
+++ validators/url/docs.ts
@@ -5,6 +5,7 @@ const exceptionUrls: string[] = [
   'https://DOGU_HOST:3001',
   'https://api.dogutech.io/v1/projects/${projectId}/applications',
   'https://api.dogutech.io/v1/projects/${projectId}/routines/${routineId}/pipelines',
+  'https://github.com/dogu-team/${props.repo_name}.git',
 ];
 const checkedUrls: { [url: string]: boolean } = {};
 const docsPath = path.join(__dirname, '../../docs');
