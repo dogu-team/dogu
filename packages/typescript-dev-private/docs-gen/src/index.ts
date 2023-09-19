8822ff478af1707009c52d1e0bc4a941b4687a6d
Byoungwook Park <qkrquddnr99@gmail.com>
2023-09-19 16:14:13 +0900

    fire: remove unused package docs-gen

--- packages/typescript-dev-private/docs-gen/src/index.ts
+++
@@ -1,4 +0,0 @@
-export { CodeGenMarkdown } from './codegen-markdown';
-export { CodeGenSyncer } from './codegen-syncer';
-export { CodeGenTypeMdxMapper } from './codegen-type-mdx-mapper';
-export * from './codegen-typescript';
