132b4dcc68c4ddf18211adc4d7a437acacd88029
Seong JuWon <lumyjuwon@gmail.com>
2023-09-20 19:32:57 +0900

    docs: move license to self-hosted

--- projects/console-web-front/enterprise/utils/license.ts
+++ projects/console-web-front/enterprise/utils/license.ts
@@ -3,7 +3,7 @@ import { LicenseBase, LicenseResponse } from '@dogu-private/console';
 export const COMMUNITY_MAX_BROWSER_COUNT = 2;
 export const COMMUNITY_MAX_MOBILE_COUNT = 2;
 
-export const LICENSE_DOCS_URL = 'https://docs.dogutech.io/get-started/installation/license';
+export const LICENSE_DOCS_URL = 'https://docs.dogutech.io/get-started/installation/self-hosted/license';
 
 export const checkExpired = (licenseInfo: LicenseBase): boolean => {
   return !!licenseInfo?.licenseToken?.expiredAt && new Date(licenseInfo.licenseToken.expiredAt) < new Date();
