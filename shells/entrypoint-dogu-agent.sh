b84f77927e004eddf9df04924355d1c5f3760aac
oneofthezombies <hunhoekim@gmail.com>
2023-09-22 15:18:17 +0900

    fix: relay DISPLAY

--- shells/entrypoint-dogu-agent.sh
+++ shells/entrypoint-dogu-agent.sh
@@ -7,10 +7,11 @@ function exit_with_error() {
 }
 
 # # Start Xvfb in the background
-# Xvfb :9 -screen 0 1920x1080x24 &
+export DISPLAY=:0
+Xvfb $DISPLAY -screen 0 1920x1080x24 &
 
 # # Get the PID of the Xvfb process
-# XVFB_PID=$!
+XVFB_PID=$!
 
 # Start the yarn dogu-agent run in the background
 yarn dogu-agent run --linux-device-serial $(uuidgen) "$@" &
@@ -18,25 +19,21 @@ yarn dogu-agent run --linux-device-serial $(uuidgen) "$@" &
 # Get the PID of the yarn dogu-agent process
 DOG_AGENT_PID=$!
 
-# exit by exit code of yarn dogu-agent
-wait $DOG_AGENT_PID
-exit $?
+# Wait for either of the processes to exit
+wait -n
 
-# # Wait for either of the processes to exit
-# wait -n
+# Get the PID of the exited process
+EXITED_PID=$!
 
-# # Get the PID of the exited process
-# EXITED_PID=$!
-
-# Determine which process exited and take appropriate action
-# if [ $EXITED_PID -eq $XVFB_PID ]; then
-#   echo "Xvfb has terminated. Terminating yarn dogu-agent."
-#   kill -TERM $DOG_AGENT_PID
-#   wait $DOG_AGENT_PID
-#   exit_with_error "yarn dogu-agent run has terminated."
-# elif [ $EXITED_PID -eq $DOG_AGENT_PID ]; then
-#   echo "yarn dogu-agent run has terminated. Terminating Xvfb."
-#   # kill -TERM $XVFB_PID
-#   wait $XVFB_PID
-#   exit_with_error "Xvfb has terminated."
-# fi
+Determine which process exited and take appropriate action
+if [ $EXITED_PID -eq $XVFB_PID ]; then
+  echo "Xvfb has terminated. Terminating yarn dogu-agent."
+  kill -TERM $DOG_AGENT_PID
+  wait $DOG_AGENT_PID
+  exit_with_error "yarn dogu-agent run has terminated."
+elif [ $EXITED_PID -eq $DOG_AGENT_PID ]; then
+  echo "yarn dogu-agent run has terminated. Terminating Xvfb."
+  kill -TERM $XVFB_PID
+  wait $XVFB_PID
+  exit_with_error "Xvfb has terminated."
+fi
