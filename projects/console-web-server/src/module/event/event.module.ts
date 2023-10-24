2d71dcbade8bef5fbaae9752e9fc1792b32f21f6
oneofthezombies <hunhoekim@gmail.com>
2023-10-24 22:16:17 +0900

    chore: add live session slack notification

--- projects/console-web-server/src/module/event/event.module.ts
+++ projects/console-web-server/src/module/event/event.module.ts
@@ -28,6 +28,7 @@ import { DeviceConnectionUpdater } from './heartbeat/device-connection-updater';
 import { HeartBeatSystemProcessor } from './heartbeat/heartbeat-system.processor';
 import { HostConnectionUpdater } from './heartbeat/host-connection-updater';
 import { LiveSessionUpdater } from './live-session-updater';
+import { MonitoringUpdater } from './monitoring-updater';
 import { DestUpdater } from './pipeline/dest-updater';
 import { DeviceJobUpdater } from './pipeline/device-job-updater';
 import { ExternalEventUpdater } from './pipeline/external-event-updater';
@@ -108,6 +109,7 @@ import { UpdateProducer } from './update-producer';
 
     LicenseUpdater,
     LiveSessionUpdater,
+    MonitoringUpdater,
   ],
   exports: [CancelPipelineQueue, UpdateStepStatusQueue, UpdateDeviceJobStatusQueue, UpdateDestStateQueue, UpdateRemoteDestStateQueue],
 })
