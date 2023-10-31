1e7e093320b473538b19ce5b226c2dc9cab89108
oneofthezombies <hunhoekim@gmail.com>
2023-10-31 16:31:26 +0900

    chore: add option

--- packages/typescript-private/console/src/base/cloud-subscription-plan.ts
+++ packages/typescript-private/console/src/base/cloud-subscription-plan.ts
@@ -1,29 +1,32 @@
 import { camelToSnakeCasePropertiesOf, propertiesOf } from '@dogu-tech/common';
-import { BillingCurrency, BillingPeriod, SubscriptionPlanPriceMap } from './billing';
+import { BillingPeriod, SubscriptionPlanPriceMap } from './billing';
 import { CloudLicenseBase } from './cloud-license';
 
 export const CloudSubscriptionPlanType = ['live-testing'] as const;
 export type CloudSubscriptionPlanType = (typeof CloudSubscriptionPlanType)[number];
 
-export interface LiveTestingCloudSubscriptionPlan {
-  type: 'live-testing';
+export interface CloudSubscriptionPlanInfo {
   optionMap: Record<number, SubscriptionPlanPriceMap>;
 }
 
-export interface CloudSubscriptionPlanInfo {
-  optionMap: Record<number, Record<BillingCurrency, LiveTestingCloudSubscriptionPlan>>;
-}
-
-// export const CloudSubscriptionPlanMap: Record<CloudSubscriptionPlanType, CloudSubscriptionPlanInfo> = {
-//   'live-testing': {
-//     priceMap: {
-//       krw: {
-//         monthly: 10000,
-//         yearly: 100000,
-//       },
-//     },
-//   },
-// };
+export const CloudSubscriptionPlanMap: Record<CloudSubscriptionPlanType, CloudSubscriptionPlanInfo> = {
+  'live-testing': {
+    optionMap: {
+      1: {
+        krw: {
+          monthly: 10000,
+          yearly: 100000,
+        },
+      },
+      2: {
+        krw: {
+          monthly: 20000,
+          yearly: 200000,
+        },
+      },
+    },
+  },
+};
 
 export interface CloudSubscriptionPlanBase {
   cloudSubscriptionPlanId: string;
