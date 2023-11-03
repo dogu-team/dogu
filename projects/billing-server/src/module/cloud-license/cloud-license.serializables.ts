a50b0f8c0e63077d35889e4b3881fb896c0b65b3
Daniel Ahn <daniel@dogutech.io>
2023-11-03 16:32:50 +0900

    fix: find cloud license

--- projects/billing-server/src/module/cloud-license/cloud-license.serializables.ts
+++ projects/billing-server/src/module/cloud-license/cloud-license.serializables.ts
@@ -1,7 +1,6 @@
 import { BillingOrganizationProp, CloudLicenseProp, CreateCloudLicenseDto, FindCloudLicenseDto } from '@dogu-private/console';
 import { ConflictException, NotFoundException } from '@nestjs/common';
 import { v4 } from 'uuid';
-import { BillingOrganization } from '../../db/entity/billing-organization.entity';
 import { CloudLicense } from '../../db/entity/cloud-license.entity';
 import { RetrySerializeContext } from '../../db/utils';
 import { createOrganization } from '../billing-organization/billing-organization.serializables';
@@ -27,13 +26,10 @@ export async function createCloudLicense(context: RetrySerializeContext, dto: Cr
 export async function findCloudLicense(context: RetrySerializeContext, dto: FindCloudLicenseDto): Promise<CloudLicense> {
   const { manager } = context;
   const { organizationId } = dto;
-  const license = await manager
-    .getRepository(CloudLicense)
-    .createQueryBuilder(CloudLicense.name)
-    .leftJoinAndSelect(`${CloudLicense.name}.${CloudLicenseProp.billingOrganization}`, CloudLicenseProp.billingOrganization)
-    .leftJoinAndSelect(`${BillingOrganization.name}.${BillingOrganizationProp.billingSubscriptionPlans}`, BillingOrganizationProp.billingSubscriptionPlans)
-    .where({ organizationId })
-    .getOne();
+  const license = await manager.getRepository(CloudLicense).findOne({
+    where: { organizationId },
+    relations: [CloudLicenseProp.billingOrganization, `${CloudLicenseProp.billingOrganization}.${BillingOrganizationProp.billingSubscriptionPlans}`],
+  });
 
   if (!license) {
     throw new NotFoundException(`Organization does not have a cloud license. organizationId: ${organizationId}`);
