311902a0ee33fcfe1364feb6db6b46ffaab10cfe
Seong JuWon <lumyjuwon@gmail.com>
2023-11-13 15:19:29 +0900

    feat: web responsive api

--- projects/console-web-server/src/db/entity/test-executor-web-responsive.entity.ts
+++ projects/console-web-server/src/db/entity/test-executor-web-responsive.entity.ts
@@ -1,4 +1,5 @@
 import { TestExecutorWebResponsiveBaseTraits, TestExecutorWebResponsivePropSnake } from '@dogu-private/console';
+import { Vendor } from '@dogu-private/device-data';
 import { TestExecutorId, TestExecutorWebResponsiveId, TestExecutorWebResponsiveUrl, TEST_EXECUTOR_WEB_RESPONSIVE_TABLE_NAME } from '@dogu-private/types';
 import { BaseEntity, Column, Entity, JoinColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 
@@ -19,13 +20,16 @@ export class TestExecutorWebResponsive extends BaseEntity implements TestExecuto
   @Column({ type: 'integer', name: TestExecutorWebResponsivePropSnake.snapshot_count, default: 0 })
   snapshotCount!: number;
 
+  @Column({ type: 'varchar', name: TestExecutorWebResponsivePropSnake.vendors, nullable: false, array: true, length: 255 })
+  vendors!: Vendor[];
+
   @ColumnTemplate.CreateDate(TestExecutorWebResponsivePropSnake.created_at)
   createdAt!: Date;
 
   @ColumnTemplate.DeleteDate(TestExecutorWebResponsivePropSnake.deleted_at)
   deletedAt!: Date | null;
 
-  @ManyToOne(() => TestExecutor, (testExecutor) => testExecutor.webResponsives, { createForeignKeyConstraints: false })
+  @ManyToOne(() => TestExecutor, (testExecutor) => testExecutor.testExecutorWebResponsives, { createForeignKeyConstraints: false })
   @JoinColumn({ name: TestExecutorWebResponsivePropSnake.test_executor_id })
   testExecutor?: TestExecutor;
 }
