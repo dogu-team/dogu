f6b23bd32827ef4a404253446096d3cc363beb8a
Byoungwook Park <qkrquddnr99@gmail.com>
2023-11-30 14:43:24 +0900

    config: disable guard temporarily to merge

--- packages/typescript-private/device-server/src/auth/guard/websocket.guard.ts
+++ packages/typescript-private/device-server/src/auth/guard/websocket.guard.ts
@@ -27,6 +27,25 @@ export function WebsocketHeaderPermission(options: PermissionOptions): MethodDec
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     const method = descriptor.value;
 
+    descriptor.value = function (): unknown {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-return, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call, prefer-rest-params
+      return method.apply(this, arguments);
+    };
+  };
+}
+
+/*
+ * @caution This decorator injects AuthService to the class instance. so you should have dependency to AuthModule in your module.
+ * Use This decorator with WebsocketIncomingMessage parameter decorator.
+ */
+export function WebsocketHeaderPermissionNotYetEnabled(options: PermissionOptions): MethodDecorator {
+  const injectAuthService = Inject(AuthService);
+
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  return (target: object, propertyKey: string | symbol, descriptor: PropertyDescriptor): void => {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const method = descriptor.value;
+
     injectAuthService(target, '__authService');
 
     descriptor.value = function (): unknown {
