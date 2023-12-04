aa053a93450518df65593665e1d6058b0bdbbe98
Byoungwook Park <qkrquddnr99@gmail.com>
2023-12-04 18:04:19 +0900

    feature/dogu-agent-auto-update

--- projects/console-web-server/src/enterprise/module/host-app/host-app.service.ts
+++ projects/console-web-server/src/enterprise/module/host-app/host-app.service.ts
@@ -1,5 +1,9 @@
-import { HostId, OrganizationId, platformArchitectureFromDownloadablePackageResult } from '@dogu-private/types';
+import { DeviceConnectionState, HostConnectionState, HostId, OrganizationId, platformArchitectureFromDownloadablePackageResult } from '@dogu-private/types';
 import { Injectable } from '@nestjs/common';
+import { InjectDataSource } from '@nestjs/typeorm';
+import { DataSource, EntityManager } from 'typeorm';
+import { Device } from '../../../db/entity/device.entity';
+import { Host } from '../../../db/entity/host.entity';
 import { DeviceMessageRelayer } from '../../../module/device-message/device-message.relayer';
 import { DownloadService } from '../../../module/download/download.service';
 import { HostService } from '../../../module/organization/host/host.service';
@@ -7,18 +11,45 @@ import { HostService } from '../../../module/organization/host/host.service';
 @Injectable()
 export class HostAppService {
   constructor(
-    private readonly hostService: HostService, //
+    @InjectDataSource()
+    private readonly dataSource: DataSource,
+    private readonly hostService: HostService,
     private readonly downloadService: DownloadService,
     private readonly deviceMessageRelayer: DeviceMessageRelayer,
   ) {}
 
+  async updateAllIdleHost(): Promise<void> {
+    await this.dataSource.transaction(async (manager) => {
+      const hosts = await manager.getRepository(Host).find({});
+      for (const host of hosts) {
+        if (host.connectionState !== HostConnectionState.HOST_CONNECTION_STATE_CONNECTED) {
+          continue;
+        }
+        if (!(await this.hostService.isHostIdle(manager, host.hostId))) {
+          continue;
+        }
+        await this.updateInternal(manager, host.organizationId, host.hostId);
+      }
+    });
+  }
+
   async update(organizationId: OrganizationId, hostId: HostId): Promise<void> {
-    const host = await this.hostService.findHost(hostId);
-    const hostDevice = host.hostDevice;
-    if (!hostDevice) {
-      throw new Error('host device not found');
+    await this.dataSource.transaction(async (manager) => {
+      await this.updateInternal(manager, organizationId, hostId);
+    });
+  }
+
+  private async updateInternal(manager: EntityManager, organizationId: OrganizationId, hostId: HostId): Promise<void> {
+    const host = await manager.getRepository(Host).findOne({ where: { hostId, connectionState: HostConnectionState.HOST_CONNECTION_STATE_CONNECTED } });
+    if (!host) {
+      throw new Error('host not found');
     }
-    const deviceId = hostDevice?.deviceId;
+
+    const anyDevice = await manager.getRepository(Device).findOne({ where: { hostId, connectionState: DeviceConnectionState.DEVICE_CONNECTION_STATE_CONNECTED } });
+    if (!anyDevice) {
+      throw new Error("host's device not found");
+    }
+    const deviceId = anyDevice?.deviceId;
     const latestApp = await this.downloadService.getDoguAgentLatest();
     const app = latestApp.find((item) => {
       const { platform, architecture } = platformArchitectureFromDownloadablePackageResult(item);
