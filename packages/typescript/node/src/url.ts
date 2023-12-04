80779efc6b45991924aeaaa12b0d98a8e080a917
Byoungwook Park <qkrquddnr99@gmail.com>
2023-12-04 20:49:37 +0900

    fix: filename maxlength handle

--- packages/typescript/node/src/url.ts
+++ packages/typescript/node/src/url.ts
@@ -2,8 +2,8 @@ import _ from 'lodash';
 import path from 'path';
 import url from 'url';
 
-export function getFilenameFromUrl(fileurl: string): string {
+export function getFilenameFromUrl(fileurl: string, maxLength = 100): string {
   const parsed = url.parse(fileurl);
-  const filename = path.basename(parsed.path ?? _.uniqueId()).substring(0, 50);
+  const filename = path.basename(parsed.path ?? _.uniqueId()).substring(0, maxLength);
   return filename;
 }
