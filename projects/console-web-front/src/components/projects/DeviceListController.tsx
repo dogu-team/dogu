f68d34cb9058b0415420e12ce407aab6b6ab6ce2
Daniel Ahn <daniel@dogutech.io>
2023-12-05 15:30:51 +0900

    Revert "temp: cookapps"
    This reverts commit 87fe29dc08b1fbe70676b45af821bcb0ff296af8.

--- projects/console-web-front/src/components/projects/DeviceListController.tsx
+++ projects/console-web-front/src/components/projects/DeviceListController.tsx
@@ -185,8 +185,7 @@ interface Props {
 
 const DeviceListController = ({ organizationId, projectId }: Props) => {
   const { data, isLoading, error, mutate, updatePage, page } = usePaginationSWR<DeviceBase>(
-    // for cookapps
-    `/organizations/${organizationId}/projects/${projectId}/devices?excludeHostIds=2c9d15d3-6852-4c34-abaa-a574ef3ce937,31b8aaab-2f73-4ab4-a908-f14642bc893e`,
+    `/organizations/${organizationId}/projects/${projectId}/devices?excludeHostIds=acb5ae68-51e2-4995-949c-da711c13bf3a`,
     {
       skipQuestionMark: true,
     },
