0468eba13a228b15c6451e6983d2ce5f07e3bad1
Daniel Ahn <daniel@dogutech.io>
2023-12-12 13:06:23 +0900

    refactor: gamium node converting

--- projects/console-web-front/src/workers/gamium-ui-tree.ts
+++ projects/console-web-front/src/workers/gamium-ui-tree.ts
@@ -2,26 +2,38 @@ import { DataNode } from 'antd/es/tree';
 import { ObjectHierarchyNode, ObjectsHierarchy } from 'gamium/common';
 
 const convertRoot = (root: ObjectsHierarchy[]) => {
-  const converted = root
+  const converted: { key: string; title: string; children: ObjectHierarchyNode[] }[] = root
     .map((item) => ({ key: `${item.name}`, title: `${item.name}`, children: item.children }))
     .filter((item) => !!item.key);
   return converted;
 };
 
-const convertNode = (node: ObjectHierarchyNode[]) => {
-  const d: DataNode[] = node.map((item) => ({
-    key: `${item.path}`,
-    title: item.name,
-    children: convertNode(item.children),
-  }));
+const convertNode = (node: ObjectHierarchyNode[], parentPath: string) => {
+  const siblingMap = new Map<string, number>();
+  const dn: DataNode[] = node.map((item) => {
+    if (siblingMap.has(item.name)) {
+      siblingMap.set(item.name, siblingMap.get(item.name)! + 1);
+    } else {
+      siblingMap.set(item.name, 1);
+    }
 
-  return d;
+    return {
+      key: `${parentPath}/${item.name}[${siblingMap.get(item.name)}]`,
+      title: item.name,
+      children: convertNode(item.children, `${parentPath}/${item.name}[${siblingMap.get(item.name)}]`),
+    };
+  });
+
+  return dn;
 };
 
 self.onmessage = (e) => {
   const data: ObjectsHierarchy[] = e.data;
   const convertedRoot = convertRoot(data);
-  const dn: DataNode[] = convertedRoot.map((item) => ({ ...item, children: convertNode(item.children) }));
+  const dn: DataNode[] = convertedRoot.map((item, i) => ({
+    ...item,
+    children: convertNode(item.children, ''),
+  }));
 
   self.postMessage(dn);
 };
