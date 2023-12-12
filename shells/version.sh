77798af72ad87ce3f511a2d1a66df211c12ea332
Daniel Ahn <daniel@dogutech.io>
2023-12-12 20:19:21 +0900

    chore: dogu version 2.8.0

--- shells/version.sh
+++ shells/version.sh
@@ -45,10 +45,6 @@ fi
 
 # Execute additional commands based on options
 if [[ $dogu == true ]]; then
-  echo "Performing additional operations for dogu"
-  sed -i "" "s|dogu:.*$|dogu:$version|" ./docker-compose.yml
-  sed -i "" "s|dogu:.*$|dogu:$version|" ./docker-compose-macOS-arm64.yml
-  sed -i "" "s|dogu:.*$|dogu:$version|" ./docker-compose-macOS-windows-x86_64.yml
   yarn version -i "$version"
   find ./projects -mindepth 1 -maxdepth 1 -type d -exec sh -c "cd {} && yarn version -i $version" \;
   exit 0
@@ -65,9 +61,6 @@ fi
 if [[ $dogu == false && $dost == false ]]; then
   echo "Performing additional operations for dogu and dost"
   yarn version -i "$version"
-  sed -i "" "s|dogu:.*$|dogu:$version|" ./docker-compose.yml
-  sed -i "" "s|dogu:.*$|dogu:$version|" ./docker-compose-macOS-arm64.yml
-  sed -i "" "s|dogu:.*$|dogu:$version|" ./docker-compose-macOS-windows-x86_64.yml
   find ./projects -mindepth 1 -maxdepth 1 -type d -exec sh -c "cd {} && yarn version -i $version" \;
   cd nm-space
   yarn workspace dogu-agent version -i "$version"
